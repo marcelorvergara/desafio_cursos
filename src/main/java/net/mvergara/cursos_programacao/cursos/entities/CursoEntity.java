5f463efb2d5bad70e2f7a1fa0a16a526a386688e
Marcelo Vergara <marcelorv@gmail.com>
2024-02-10 13:58:34 -0300

    Not blank fields

--- src/main/java/net/mvergara/cursos_programacao/cursos/entities/CursoEntity.java
+++ src/main/java/net/mvergara/cursos_programacao/cursos/entities/CursoEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,7 +19,9 @@ public class CursoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @NotBlank(message="O campo [name] não deve estar em branco")
     private String name;
+    @NotBlank(message="O campo [category] não deve estar em branco")
     @Pattern(regexp = "\\S+", message="O campo [category] não deve conter espaço")
     private String category;
     private boolean active;
